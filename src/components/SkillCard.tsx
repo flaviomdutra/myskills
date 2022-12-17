fba97877182206186e374b05353d4c283d07cdea
Flávio Dutra <flaviomdutra@outlook.com>
2022-12-17 17:17:22 -0300

    Initial commit

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,33 @@
+import {
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+} from "react-native";
+
+interface SkillCardProps extends TouchableOpacityProps {
+  skill: string;
+}
+
+export function SkillCard({ skill, ...rest }: SkillCardProps) {
+  return (
+    <TouchableOpacity style={styles.buttonSkill} {...rest}>
+      <Text style={styles.textSkill}>{skill}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonSkill: {
+    padding: 15,
+    backgroundColor: "#1F1E25",
+    borderRadius: 50,
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  textSkill: {
+    color: "#FFF",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
